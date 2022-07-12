6f5b4840d9456854edaaf5d15df77bb45bf304fd
Richard-Gaspar <richard.j.gaspar@gmail.com>
2022-07-12 11:54:50 +0200

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -100,43 +100,36 @@ for (let i = 0; i < colorArray.length; i++) {
   colorsArray.push(colorArray[i].dataset.color);
 }
 
-let secretColorArray = [
-  {
-    color: (secretColor1.id = colorsArray[Math.floor(Math.random() * 6)]),
-    is: false,
-  },
-  {
-    color: (secretColor2.id = colorsArray[Math.floor(Math.random() * 6)]),
-    is: false,
-  },
-  {
-    color: (secretColor3.id = colorsArray[Math.floor(Math.random() * 6)]),
-    is: false,
-  },
-  {
-    color: (secretColor4.id = colorsArray[Math.floor(Math.random() * 6)]),
-    is: false,
-  },
-];
+let secretColorArray = [];
 
-// let secretColorArray = [];
+const shuffleColors = function () {
+  secretColorArray.push(
+    {
+      color: (secretColor1.id = colorsArray[Math.floor(Math.random() * 6)]),
+      found: false,
+    },
+    {
+      color: (secretColor2.id = colorsArray[Math.floor(Math.random() * 6)]),
+      found: false,
+    },
+    {
+      color: (secretColor3.id = colorsArray[Math.floor(Math.random() * 6)]),
+      found: false,
+    },
+    {
+      color: (secretColor4.id = colorsArray[Math.floor(Math.random() * 6)]),
+      found: false,
+    }
+  );
+};
 
-// const shuffleColors = function () {
-//   secretColorArray.push(
-//     (secretColor1.id = colorsArray[Math.floor(Math.random() * 6)]),
-//     (secretColor2.id = colorsArray[Math.floor(Math.random() * 6)]),
-//     (secretColor3.id = colorsArray[Math.floor(Math.random() * 6)]),
-//     (secretColor4.id = colorsArray[Math.floor(Math.random() * 6)])
-//   );
-// };
-// shuffleColors();
+shuffleColors();
+// console.log(secretColor1.id, secretColor2.id, secretColor3.id, secretColor4.id);
 
-console.log(secretColor1.id, secretColor2.id, secretColor3.id, secretColor4.id);
-
-const circles = document.querySelectorAll(".guess-dots");
-const circlesArray = Array.from(circles);
-const checkDots = document.querySelectorAll(".check-dots");
-const checkDotsArray = Array.from(checkDots);
+let circles = document.querySelectorAll(".guess-dots");
+let circlesArray = Array.from(circles);
+let checkDots = document.querySelectorAll(".check-dots");
+let checkDotsArray = Array.from(checkDots);
 
 let activeRow;
 let activeCheck;
@@ -167,33 +160,6 @@ const deleteActiveDot = function () {
 makeActiveRow();
 makeActiveDot();
 
-colors.forEach((colors) =>
-  colors.addEventListener("click", function () {
-    if (!activeRow.some((element) => element.style.backgroundColor === ""))
-      return;
-    const color = colors.getAttribute("data-color");
-    console.log(color);
-    if (activeRow[0].style.backgroundColor === "") {
-      activeRow[0].style.backgroundColor = `${color}`;
-    } else if (activeRow[1].style.backgroundColor === "") {
-      activeRow[1].style.backgroundColor = `${color}`;
-    } else if (activeRow[2].style.backgroundColor === "") {
-      activeRow[2].style.backgroundColor = `${color}`;
-    } else if (activeRow[3].style.backgroundColor === "") {
-      activeRow[3].style.backgroundColor = `${color}`;
-    }
-
-    deleteActiveDot();
-    makeActiveDot();
-  })
-);
-
-// activeCheck[
-//   activeCheck.findIndex((element) => (element.style.backgroundColor = ""))
-// ].style.backgroundColor = "black";
-
-//////// BUG =====> Ha egy szín jó helyen van, akkor is fehér pöttyöt rak ha egy ugyanolyan színt pakolok bele 2.nak
-
 let pegsOrder = [];
 
 const pegOrderFunction = function () {
@@ -211,67 +177,43 @@ const pegOrderFunction = function () {
   activeCheck[3].style.backgroundColor = `${pegsOrder[3]}`;
 };
 
-// const inventory = [
-//   { position: 1, is: false },
-//   { position: 2, is: false },
-//   { position: 3, is: false },
-//   { position: 4, is: false },
-// ];
+const checkElement = function () {
+  // debugger;
+  activeRow.forEach((element, index) => {
+    if (element.style.backgroundColor === secretColorArray[index].color) {
+      secretColorArray[index].found = true;
+      pegsOrder.push("black");
+    }
+  });
 
-const checkElement = function (index) {
-  if (
-    activeRow[index].style.backgroundColor === secretColorArray[index].color
-  ) {
-    pegsOrder.push("black");
-    secretColorArray[index].is = true;
-  } else if (
-    secretColorArray[0].color === activeRow[index].style.backgroundColor &&
-    secretColorArray[0].is === false
-  ) {
-    secretColorArray[0].is === true;
-    pegsOrder.push("white");
-    console.log(secretColorArray, "ELSŐ");
-  } else if (
-    secretColorArray[1].color === activeRow[index].style.backgroundColor &&
-    secretColorArray[1].is === false
-  ) {
-    secretColorArray[1].is === true;
-    pegsOrder.push("white");
-    console.log(secretColorArray, "MÁSODIK");
-  } else if (
-    secretColorArray[2].color === activeRow[index].style.backgroundColor &&
-    secretColorArray[2].is === false
-  ) {
-    secretColorArray[2].is === true;
-    pegsOrder.push("white");
-    console.log(secretColorArray, "HARMADIK");
-  } else if (
-    secretColorArray[3].color === activeRow[index].style.backgroundColor &&
-    secretColorArray[3].is === false
-  ) {
-    secretColorArray[2].is === true;
-    pegsOrder.push("white");
-    console.log(secretColorArray, "NEGYEDIK");
-  }
-  secretColorArray[0].is = false;
-  secretColorArray[1].is = false;
-  secretColorArray[2].is = false;
-  secretColorArray[3].is = false;
+  activeRow.forEach((element, index) => {
+    if (
+      element.style.backgroundColor !== secretColorArray[index].color &&
+      secretColorArray.some(
+        (el) => el.color === element.style.backgroundColor && el.found !== true
+      )
+    ) {
+      secretColorArray.find(
+        (el) => el.color === element.style.backgroundColor && el.found !== true
+      ).found = true;
+      pegsOrder.push("white");
+    }
+  });
+  secretColorArray[0].found = false;
+  secretColorArray[1].found = false;
+  secretColorArray[2].found = false;
+  secretColorArray[3].found = false;
 };
 
-// should make an error if dots not filled all the way
 checkMyGuess.addEventListener("click", function () {
   if (activeRow.some((element) => element.style.backgroundColor === "")) {
     alert("You need to fill all dots");
     return;
   }
-  checkElement(0);
-  checkElement(1);
-  checkElement(2);
-  checkElement(3);
+  checkElement();
 
   pegOrderFunction();
-  // console.log(pegsOrder);
+
   // Win game condition
   if (
     activeRow[0].style.backgroundColor === secretColorArray[0].color &&
@@ -312,7 +254,6 @@ const deleteColor = function () {
 // Last dot bug
 deleteBtn.addEventListener("click", function () {
   deleteColor();
-  console.log(activeRow[3].style.backgroundColor);
   if (activeRow[3].style.backgroundColor === "") {
     makeActiveDot();
   }
@@ -323,6 +264,45 @@ deleteBtn.addEventListener("click", function () {
       .find((element) => element.classList.contains("current"))
       .classList.remove("current");
   }
-
-  console.log("haho");
 });
+
+newGame.addEventListener("click", function () {
+  while (leftSide.firstChild) {
+    leftSide.removeChild(leftSide.firstChild);
+  }
+  while (rightSide.firstChild) {
+    rightSide.removeChild(rightSide.firstChild);
+  }
+
+  secretColorArray = [];
+  makeDivs();
+  shuffleColors();
+
+  circles = document.querySelectorAll(".guess-dots");
+  circlesArray = Array.from(circles);
+  checkDots = document.querySelectorAll(".check-dots");
+  checkDotsArray = Array.from(checkDots);
+
+  makeActiveRow();
+  makeActiveDot();
+});
+
+colors.forEach((colors) =>
+  colors.addEventListener("click", function () {
+    if (!activeRow.some((element) => element.style.backgroundColor === ""))
+      return;
+    const color = colors.getAttribute("data-color");
+    if (activeRow[0].style.backgroundColor === "") {
+      activeRow[0].style.backgroundColor = `${color}`;
+    } else if (activeRow[1].style.backgroundColor === "") {
+      activeRow[1].style.backgroundColor = `${color}`;
+    } else if (activeRow[2].style.backgroundColor === "") {
+      activeRow[2].style.backgroundColor = `${color}`;
+    } else if (activeRow[3].style.backgroundColor === "") {
+      activeRow[3].style.backgroundColor = `${color}`;
+    }
+
+    deleteActiveDot();
+    makeActiveDot();
+  })
+);
